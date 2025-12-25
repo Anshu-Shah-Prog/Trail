f12160f73e7451e90949e9085a0a1d0bfa1d593d
Anshu-Shah-Prog <anshshah7643@gmail.com>
2025-12-25 13:49:52 +0530

    Update App.py

--- App.py
+++ App.py
@@ -97,6 +97,15 @@ if st.session_state.page == 1:
 elif st.session_state.page == 2:
     render_section("A", ["A1", "A2", "A3", "A4", "A5", "A6", "A7"], 3)
 elif st.session_state.page == 3:
-    render_section("B", ["B1", "B2", "B3"], 8) # Example jump to final
+    render_section("B", ["B1", "B2", "B3"], 4) # Example jump to final
+elif st.session_state.page == 4:
+    render_section("C", ["C1","C2","C3","C4","C5",
+                "C6","C7","C8","C9","C10","C11","C12"], 5)
+elif st.session_state.page == 5:
+    render_section("D", ["D1","D2","D3","D4","D5","D6","D7","D8","D9"], 6) # Example jump to final
+elif st.session_state.page == 6:
+    render_section("E", ["E1","E2","E3","E4"], 7)
+elif st.session_state.page == 7:
+    render_section("F", ["F1","F2","F3","F4","F5","F6"], 8) # Example jump to final
 elif st.session_state.page == 8:
     show_final()
